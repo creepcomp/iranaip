8414511e383a7161f1ff84159e38911614025aa9
Creepcomp <creepcomp@gmail.com>
2025-12-29 18:07:35 +0330

    Fix: token can be null.

--- lib/isAdmin.ts
+++ lib/isAdmin.ts
@@ -1,6 +1,7 @@
 import { cookies } from 'next/headers';
 
 export async function isAdmin() {
-  const secret = (await cookies()).get("token").value;
+  const token = (await cookies()).get("token");
+  const secret = token?.value;
   return secret === process.env.ADMIN_TOKEN;
 }
